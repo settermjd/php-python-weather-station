0b1a1cc047f1596ffc08780f78543ab1d3a31e11
Matthew Setter <matthew@matthewsetter.com>
2022-03-22 15:35:51 +0100

    Add a WeatherService service
    The intention of this class is to encapsulate the weather data
    functionality within one class, functionality such as retrieving
    weather data records.

---
+++ src/Service/WeatherService.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WeatherStation\Service;
+
+use Laminas\Db\{Adapter\Adapter,
+    ResultSet\ResultSetInterface,
+    Sql\Predicate\Between,
+    Sql\Predicate\Expression,
+    Sql\Sql};
+
+class WeatherService
+{
+    private Adapter $adapter;
+
+    public function __construct(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    public function getWeatherData(?string $forDate, ?string $endDate): ResultSetInterface
+    {
+        $sql = new Sql($this->adapter, 'weather_data');
+        $select = $sql
+            ->select()
+            ->columns(['humidity', 'temperature', 'timestamp']);
+
+        if ($forDate !== null && $endDate !== null) {
+            $select->where(new Between("date(timestamp)", $forDate, $endDate));
+        } else {
+            if ($forDate !== null) {
+                $select->where(new Expression("date(timestamp) = ?", $forDate));
+            }
+        }
+
+        return $this
+            ->adapter
+            ->query(
+                $sql->buildSqlString($select),
+                $this->adapter::QUERY_MODE_EXECUTE
+            );
+    }
+}
