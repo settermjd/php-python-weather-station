1306ce280a0832a38f58e4a2b5bd29c465f069ec
Matthew Setter <matthew@matthewsetter.com>
2022-04-04 10:36:05 +0200

    Add the ability to send status notifications
    This change:
    
    - Adds functions to send a daily summary email and daily summary SMS
    - Adds a route that retrieves weather data and sends the summaries
    - Adds a download route, so that the weather data can be downloaded in
    CSV file format

--- public/index.php
+++ public/index.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 use DI\Container;
 use Laminas\Db\Adapter\Adapter;
-use Laminas\Db\Sql\Sql;
-use Laminas\Diactoros\Response\TextResponse;
+use Laminas\Db\ResultSet\ResultSet;
+use Laminas\Db\ResultSet\ResultSetInterface;
+use Laminas\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\Response\JsonResponse;
+use Laminas\Diactoros\Stream;
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
@@ -13,9 +16,12 @@ use Psr\Http\Message\{
     ResponseInterface as Response,
     ServerRequestInterface as Request
 };
+use SendGrid\Mail\Mail;
 use Slim\Factory\AppFactory;
 use Slim\Views\{Twig,TwigMiddleware};
 use Twig\Extra\Intl\IntlExtension;
+use Twilio\Rest\Client;
+use WeatherStation\Service\WeatherService;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -41,7 +47,7 @@ $container->set('dbAdapter', function() {
     ]);
 });
 
-$container->set('weatherService', function(ContainerInterface $container) {
+$container->set(WeatherService::class, function(ContainerInterface $container) {
     return new WeatherStation\Service\WeatherService(
         $container->get('dbAdapter'),
         $container->get(LoggerInterface::class)
@@ -64,46 +70,138 @@ AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->add(TwigMiddleware::createFromContainer($app));
 
-$app->map(['GET'], '/[{forDate}]', function (Request $request, Response $response, array $args) {
-    /** @var WeatherStation\Service\WeatherService $weatherService */
-    $weatherService = $this->get('weatherService');
-    $forDate = $request->getAttribute('forDate');
-
-    /** @var Logger $logger */
-    $logger = $this->get('logger');
-    $logger->debug('Requested data', ['for date' => $forDate]);
-
-    return $this->get('view')->render(
-        $response,
-        'index.html.twig',
-        ['items' => $weatherService->getWeatherData($forDate)]
-    );
-});
-
-$app->map(['GET'], '/download[/{forDate}]', function (Request $request, Response $response, array $args) {
-    /** @var WeatherStation\Service\WeatherService $weatherService */
-    $weatherService = $this->get('weatherService');
-    $forDate = $request->getAttribute('forDate');
-    $weatherData = $weatherService->getWeatherData($forDate);
-    $filename = 'daily-summary';
-
+function createCsvFileFromWeatherData(ResultSetInterface $weatherData): Stream
+{
     $fp = fopen('php://memory', 'rw');
     foreach ($weatherData->toArray() as $weatherDatum) {
         fputcsv($fp, $weatherDatum);
     }
-    $stream = new \Laminas\Diactoros\Stream($fp);
+    $stream = new Stream($fp);
+    $stream->rewind();
 
-    return new TextResponse(
-        $stream,
-        200,
-        [
-            'Content-Type' => 'text/csv',
-            'Content-Disposition' => "attachment; filename={$filename}.csv",
-            'Content-Length' => $stream->getSize(),
-            'Pragma' => 'no-cache',
-            'Expires' => 0,
-        ]
+    return $stream;
+}
+
+function sendDailySummaryEmail(string $startDate, string $endDate, ResultSet $weatherData)
+{
+    $email = new Mail();
+    $email->setFrom($_SERVER['SENDGRID_SEND_FROM'], "Matthew Setter");
+    $email->setSubject("Your daily weather station data summary");
+    $email->addTo("msetter@twilio.com", "Matthew Setter");
+    $email->addContent(
+        "text/plain",
+        sprintf(
+            "Your daily weather station data summary for %s - %s",
+            $startDate,
+            $endDate
+        )
     );
+    $email->addContent(
+        "text/html",
+        sprintf(
+            "Your daily weather station data summary for %s - %s",
+            $startDate,
+            $endDate
+        )
+    );
+    $stream = createCsvFileFromWeatherData($weatherData);
+    $file_encoded = base64_encode($stream->getContents());
+    $email->addAttachment(
+        $file_encoded,
+        "text/csv",
+        "daily-summary.csv",
+        "attachment"
+    );
+    $sendgrid = new \SendGrid($_SERVER['SENDGRID_API_KEY']);
+    try {
+        $sendgrid->send($email);
+    } catch (Exception $e) {
+        echo 'Caught exception: ' . $e->getMessage() . "\n";
+    }
+}
+
+/**
+ * @throws \Twilio\Exceptions\ConfigurationException
+ * @throws \Twilio\Exceptions\TwilioException
+ */
+function sendDailySummarySMS(string $startDate, string $endDate): void
+{
+    $twilio = new Client($_SERVER["TWILIO_ACCOUNT_SID"], $_SERVER["TWILIO_AUTH_TOKEN"]);
+    $twilio
+        ->messages
+        ->create(
+            $_SERVER["SEND_TO"],
+            [
+                "messagingServiceSid" => $_SERVER["TWILIO_MESSAGING_SERVICE_SID"],
+                "body" => sprintf(
+                    "Your daily weather station data summary is available at http://localhost/daily-summary/%s/%s",
+                    $startDate,
+                    $endDate
+                ),
+            ]
+        );
+}
+
+$app->get('/daily-summary[/{startDate}[/{endDate}]]', function (Request $request, Response $response, array $args) {
+    /** @var WeatherStation\Service\WeatherService $weatherService */
+    $weatherService = $this->get(WeatherService::class);
+    $startDate = $request->getAttribute('startDate');
+    $endDate = $request->getAttribute('endDate');
+    $weatherData = $weatherService->getWeatherData($startDate, $endDate);
+
+    if ($weatherData->count() === 0) {
+        return new JsonResponse('No weather data available for that date range.');
+    }
+
+    sendDailySummarySMS($startDate, $endDate);
+    sendDailySummaryEmail($startDate, $endDate, $weatherData);
+
+    return new EmptyResponse();
+})->setName('daily-summary');
+
+$app->map(['GET','POST'], '/[{startDate}[/{endDate}]]', function (Request $request, Response $response, array $args) {
+    /** @var WeatherStation\Service\WeatherService $weatherService */
+    $weatherService = $this->get(WeatherService::class);
+    $startDate = $request->getAttribute('startDate');
+    $endDate = $request->getAttribute('endDate');
+
+    //$pageNumber = $request->getAttribute('page', 1);
+
+    $this->get(LoggerInterface::class)
+        ->debug(
+            'Requested data',
+            [
+                'start date' => $startDate ?? 'not supplied',
+                'end date' => $endDate ?? 'not supplied'
+            ]
+        );
+
+    $weatherData = $weatherService->getWeatherData($startDate, $endDate);
+
+    if ($request->hasHeader("content-type") && $request->getHeaderLine("content-type") === 'text/csv') {
+        $csvFileFromWeatherData = createCsvFileFromWeatherData($weatherData);
+        return new JsonResponse(
+            [
+                'data' => $csvFileFromWeatherData->getContents(),
+                'size' => $csvFileFromWeatherData->getSize(),
+            ]
+        );
+    }
+
+    /*$paginator = new Paginator(new PaginatorIterator($weatherData));*/
+    return $this
+        ->get('view')
+        ->render(
+            $response,
+            'index.html.twig',
+            [
+                'items' => $weatherData,
+                /*'items' => $paginator->getItemsByPage($pageNumber),
+                'total' => 10,
+                'current' => $pageNumber,
+                'url' => 'page'*/
+            ]
+        );
 });
 
 $app->run();
